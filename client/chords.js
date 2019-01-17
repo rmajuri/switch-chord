f338a414e5d85a58240dfc1f54fdcdfb8f60438e
Rob Majuri <robmajuri@gmail.com>
2019-01-17 17:35:10 -0500

    about to add key toggling

--- client/chords.js
+++ client/chords.js
@@ -20,7 +20,6 @@ const gMajor = new chordObj("G3", "B3", "D4")
 const dMinor = new chordObj("D3", "F3", "A3")
 const eMinor = new chordObj("E3", "G3", "B3")
 const bDim = new chordObj("B3", "D4", "F4")
-const bDim7 = new chordObj("B3", "D4", "F4", "A4")
 const cMajorMaj7 = new chordObj("C3", "E3", "G3", "B3")
 const cMajorMaj9 = new chordObj("C3", "E3", "G3", "B3", "D4")
 const aMinor7 = new chordObj("A3", "C4", "E4", "G4")
@@ -28,7 +27,6 @@ const fMajorMaj7 = new chordObj("F3", "A3", "C4", "E4")
 const fMajorMaj9 = new chordObj("F3", "A3", "C4", "E4", "G4")
 const gMajor7 = new chordObj("G3", "B3", "D4", "F4")
 const dMinor7 = new chordObj("D3", "F3", "A3", "C4")
-const dMinor6 = new chordObj("D3", "F3", "A3", "B3")
 const dMinor9 = new chordObj("D3", "F3", "A3", "C4", "E4")
 const eMinor7 = new chordObj("E3", "G3", "B3", "D4")
 const eMinor9 = new chordObj("E3", "G3", "B3", "D4", "F4")
@@ -36,8 +34,6 @@ const cMajorAdd9 = new chordObj("C3", "E3", "G3", "D4")
 const gMajorAdd9 = new chordObj("G3", "B3", "D4", "A4")
 const fMajorAdd9 = new chordObj("F3", "A3", "C4", "G4")
 const aMinor9 = new chordObj("A3", "C4", "E4", "G4", "B4")
-const eMinor6 = new chordObj("E3", "G3", "B3", "C4")
-const aMinor6 = new chordObj("A3", "C4", "E4", "F4")
 const C5 = new chordObj("C3", "G3")
 const D5 = new chordObj("D3", "A3")
 const E5 = new chordObj("E3", "B3")
@@ -49,9 +45,7 @@ const fMajor6 = new chordObj("F3", "A3", "C4", "D4")
 const gMajor6 = new chordObj("G3", "B3", "D4", "E4")
 const gMajor9 = new chordObj("G3", "B3", "D4", "F4", "A4")
 const dMinorAdd9 = new chordObj("D3", "F3", "A3", "E4")
-const eMinorAdd11 = new chordObj("E3", "G3", "B3", "A4")
 const aMinorAdd9 = new chordObj("A3", "C4", "E4", "B4")
-
 
 
 const cMajorScaleChords = {}
@@ -62,45 +56,34 @@ cMajorScaleChords.CMaj7 = cMajorMaj7
 cMajorScaleChords.CMaj9 = cMajorMaj9
 cMajorScaleChords.Cadd9 = cMajorAdd9
 cMajorScaleChords.C6 = cMajor6
-
-
 cMajorScaleChords.D5 = D5
 cMajorScaleChords.Dm = dMinor
-cMajorScaleChords.Dm6 = dMinor6
 cMajorScaleChords.Dm7 = dMinor7
 cMajorScaleChords.Dm9 = dMinor9
 cMajorScaleChords.Dmadd9 = dMinorAdd9
-
 cMajorScaleChords.E5 = E5
 cMajorScaleChords.Em = eMinor
-cMajorScaleChords.Em6 = eMinor6
 cMajorScaleChords.Em7 = eMinor7
 cMajorScaleChords.Em9 = eMinor9
-cMajorScaleChords.Em11 = eMinorAdd11
-
 cMajorScaleChords.F5 = F5
 cMajorScaleChords.F = fMajor
 cMajorScaleChords.FMaj7 = fMajorMaj7
 cMajorScaleChords.FMaj9 = fMajorMaj9
 cMajorScaleChords.Fadd9 = fMajorAdd9
 cMajorScaleChords.F6 = fMajor6
-
 cMajorScaleChords.G5 = G5
 cMajorScaleChords.G = gMajor
 cMajorScaleChords.G7 = gMajor7
 cMajorScaleChords.Gadd9 = gMajorAdd9
 cMajorScaleChords.G6 = gMajor6
 cMajorScaleChords.G9 = gMajor9
-
 cMajorScaleChords.A5 = A5
 cMajorScaleChords.Am = aMinor
-cMajorScaleChords.Am6 = aMinor6
 cMajorScaleChords.Am7 = aMinor7
 cMajorScaleChords.Am9 = aMinor9
 cMajorScaleChords.Amadd9 = aMinorAdd9
-
 cMajorScaleChords.Bdim = bDim
-cMajorScaleChords.Bdim7 = bDim7
+
 
 
 export default cMajorScaleChords
