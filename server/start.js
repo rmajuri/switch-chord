116888e6eda3ef4db267aeceafee2b5e0f0eaddb
Rob Majuri <robmajuri@gmail.com>
2019-01-21 15:18:51 -0500

    deploying in process

--- server/start.js
+++ server/start.js
@@ -2,7 +2,5 @@
 const app = require('./index');
 const port = process.env.PORT || 3000;
 
-
-    app.listen(port) // then start listening with our express server once we have synced
-
+app.listen(port)
 
